fd9c1b4f4da7425f0b7c23d0d414bee21d98fc6b
Zack Allen <zack.m.allen@gmail.com>
2018-07-21 11:53:08 -0700

    made some changes

--- ViewController.cs
+++ ViewController.cs
@@ -13,8 +13,10 @@ using Esri.ArcGISRuntime.Security;
 using Esri.ArcGISRuntime.Symbology;
 using Esri.ArcGISRuntime.Tasks;
 using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.UI.Controls;
 using UIKit;
+using System.Drawing;
 
 namespace HackathoughtsApp
 {
@@ -22,6 +24,11 @@ namespace HackathoughtsApp
     {
         MapViewModel _mapViewModel = new MapViewModel();
         MapView _mapView;
+        private MapPoint _lostLocation;
+        private GraphicsOverlay _bufferOverlay;
+        private GraphicsOverlay _barrierOverlay;
+        private GraphicsOverlay _interestOverlay;
+        private GraphicsOverlay _lostOverlay;
 
         public ViewController(IntPtr handle) : base(handle)
         {
@@ -39,6 +46,35 @@ namespace HackathoughtsApp
                 Map = _mapViewModel.Map // Use the map from the view-model
             };
 
+            _mapView.GeoViewTapped += MyMapView_GeoViewTapped;
+
+            //Create graphics overlays and add them
+            _bufferOverlay = new GraphicsOverlay();
+            _barrierOverlay = new GraphicsOverlay();
+            _interestOverlay = new GraphicsOverlay();
+            _lostOverlay = new GraphicsOverlay();
+
+            _mapView.GraphicsOverlays.Add(_bufferOverlay);
+            _mapView.GraphicsOverlays.Add(_barrierOverlay);
+            _mapView.GraphicsOverlays.Add(_interestOverlay);
+            _mapView.GraphicsOverlays.Add(_lostOverlay);
+
+            // location where user marks dog lost
+            //_lostLocation = new MapPoint();
+
+            //get redlands boundary
+
+            // Create URI to the used feature service.
+            Uri serviceUri = new Uri("https://services.arcgis.com/FLM8UAw9y5MmuVTV/ArcGIS/rest/services/CityLimits_Redlands/FeatureServer/0");
+
+            // Create new FeatureLayer by URL.
+            FeatureLayer redlandsBoundary = new FeatureLayer(serviceUri);
+
+            // Add layer to the map.
+            _mapView.Map.OperationalLayers.Add(redlandsBoundary);
+
+            
+
             // Add the MapView to the Subview
             View.AddSubview(_mapView);
         }
@@ -49,6 +85,17 @@ namespace HackathoughtsApp
             _mapView.Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width, View.Bounds.Height);
 
             base.ViewDidLayoutSubviews();
+        }
+
+        private async void MyMapView_GeoViewTapped(object sender, GeoViewInputEventArgs e)
+        {
+            // Get the tapped point.
+            _lostLocation = e.Location;
+
+            _lostOverlay.Graphics.Clear();
+            _lostOverlay.Graphics.Add(new Graphic(_lostLocation, new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.X, Color.Blue, 30.0)));
+
+
         }
 
         private void MapViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
